be38ecd54a3e9552af9c184f30234656a622b915
md-hassan-1 <hassanansari8533@gmail.com>
2024-02-05 16:35:33 +0530

    creat project

---
+++ min.js
@@ -0,0 +1,9 @@
+ let change = new Typed("#type",{
+    String:["frontend develoer","youtuber","web developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+
+ });
+ 
