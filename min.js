57ca81f89fd36ee76cb5ccdc9389676f1bb576fd
md-hassan-1 <hassanansari8533@gmail.com>
2024-02-06 11:53:02 +0530

    changed in the file

--- min.js
+++ min.js
@@ -1,5 +1,5 @@
- let change = new Typed("#type",{
-    String:["frontend develoer","youtuber","web developer"],
+ var typed = new Typed(".text",{
+    String:["frontend develoer" , "youtuber" , "web developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
